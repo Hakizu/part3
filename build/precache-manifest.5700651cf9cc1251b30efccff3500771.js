43467f030d2a0ff4aba68369648f6db6516dfc00
Hakizu <hakisuj@gmail.com>
2020-05-27 11:57:11 +0200

    pushing build

---
+++ build/precache-manifest.5700651cf9cc1251b30efccff3500771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbd74fd906022bd19b1d9810bb343f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd32e20914203fd4e7ee",
+    "url": "/static/css/main.c80c82b4.chunk.css"
+  },
+  {
+    "revision": "786a3392310240a837fb",
+    "url": "/static/js/2.f7c60931.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7c60931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd32e20914203fd4e7ee",
+    "url": "/static/js/main.f52c0cf8.chunk.js"
+  },
+  {
+    "revision": "4aaf74b7fb1ed205224c",
+    "url": "/static/js/runtime-main.48c7fc5f.js"
+  }
+]);
