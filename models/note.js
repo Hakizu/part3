32c3c58aedb628ec90b9fc1b66cedda70030b060
Hakizu <hakisuj@gmail.com>
2020-05-27 21:47:33 +0200

    Deploy

--- models/note.js
+++ models/note.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = process.env.MONGODB_URI
+const url = 'mongodb+srv://helios:selene@cluster0-z0nly.mongodb.net/test?retryWrites=true&w=majority'
 
 mongoose.set('useFindAndModify', false)
 console.log('connecting to', url)
