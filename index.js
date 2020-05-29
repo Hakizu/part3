0aecddbd8d9dd80eb6e10e139cb8d89e76af88ab
Hakizu <hakisuj@gmail.com>
2020-05-29 13:41:27 +0200

    Follow along for part3 fully completed

--- index.js
+++ index.js
@@ -9,90 +9,90 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
-app.get('/api/notes' , (request, response) => { 
-    Note.find({}).then(notes => {
-        response.json(notes.map(note => note.toJSON()))
-    })
-})       
+app.get('/api/notes' , (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes.map(note => note.toJSON()))
+  })
+})
 
 app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if  (body.content === undefined) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
+  if  (body.content === undefined) {
+    return response.status(400).json({
+      error: 'content missing'
     })
+  }
 
-    note.save()
-        .then(savedNote => response.json(savedNote.toJSON()))
-        .then(savedAndFormattedNote => 
-            response.json(savedAndFormattedNote))
-        .catch(error => next(error))
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note.save()
+    .then(savedNote => response.json(savedNote.toJSON()))
+    .then(savedAndFormattedNote =>
+      response.json(savedAndFormattedNote))
+    .catch(error => next(error))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note.toJSON())
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note.toJSON())
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
-  
-    const note = {
-      content: body.content,
-      important: body.important,
-    }
-  
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-      .then(updatedNote => {
-        response.json(updatedNote.toJSON())
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
     })
-      .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.use(unknownEndpoint)    
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
+  console.log(error.message)
 
-    if (error.name === 'CastError' && error.kind == 'ObjectId') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({error: error})
-    }
-    next(error)
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error })
+  }
+  next(error)
 }
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`)
+  console.log(`Server running on port: ${PORT}`)
 })
--- models/note.js
+++ models/note.js
@@ -5,7 +5,7 @@ mongoose.set('useFindAndModify', false)
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -28,11 +28,11 @@ const noteSchema = new mongoose.Schema({
 })
 
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
