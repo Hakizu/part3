ce70836d6e3e4f7eb05a4c5c9ab82f4701c4883b
Hakizu <hakisuj@gmail.com>
2020-06-04 18:37:18 +0200

    Follow along part4 until refactoring tests

--- test/note_api.test.js
+++ test/note_api.test.js
@@ -9,11 +9,11 @@ const Note = require('../models/note')
 beforeEach(async () => {
   await Note.deleteMany({})
 
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  const noteObjects = helper.initialNotes
+    .map(note => new Note(note))
+  const promiseArray = noteObjects.map(note =>
+    note.save())
+  await Promise.all(promiseArray)
 })
 
 
@@ -74,6 +74,36 @@ test('note without content is not added', async () => {
   const notesAtend = await helper.notesInDb()
 
   expect(notesAtend).toHaveLength(helper.initialNotes.length)
+})
+
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(resultNote.body).toEqual(noteToView)
+})
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const notesToDelete = notesAtStart[0]
+
+  await api
+    .delete(`/api/notes/${notesToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+  expect(notesAtEnd).toHaveLength(
+    helper.initialNotes.length - 1
+  )
+
+  const contents = notesAtEnd.map(r => r.content)
+  expect(contents).not.toContain(notesToDelete.content)
 })
 
 afterAll(() => {
